cec6328bfc6bee9dde9b71519b2b7a378ea046b8
Saulo <saulomcm@gmail.com>
2023-10-01 14:19:12 -0300

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+  itens: any[] = []
   constructor() {}
 
+  ngOnInit(): void {
+    this.itens = [
+      {
+        qtd: 10,
+        descricao: 'Arroz'
+      },
+      {
+        qtd: 12,
+        descricao: 'Feijão'
+      },
+      {
+        qtd: 3,
+        descricao: 'Açucar'
+      }
+    ]
+  }
 }
