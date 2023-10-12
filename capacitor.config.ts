4c768f204a07357e358da8cb10695b45de937dc3
Saulo Maciel <saulomcm@gmail.com>
2023-10-12 11:04:04 -0300

    Altera o appID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.com.ionic.listadecompras',
   appName: 'ListaDeCompras',
   webDir: 'www',
   server: {
