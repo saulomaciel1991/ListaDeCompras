acd50c53f6d11f7d9a41740d13bed060da040239
Saulo Maciel <saulomcm@gmail.com>
2024-01-18 11:17:10 -0300

    Atualiza o nome do APP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'br.com.ionic.listadecompras',
-  appName: 'ListaDeCompras',
+  appName: 'Lista de Compras',
   webDir: 'www',
   server: {
     androidScheme: 'https'
